72dbd9abe18b6a1cb788a14fb383004b48745df9
Rockey Simmons <codenamerockey@gmail.com>
2019-09-23 13:07:23 -0700

    updated base url to the api being built for the project

--- replate/src/utils/axiosWithAuth.js
+++ replate/src/utils/axiosWithAuth.js
@@ -9,7 +9,7 @@ export const axiosWithAuth = () => {
   const token = localStorage.getItem('token');
 
   return axios.create({
-    baseURL: 'http://localhost:5000/api',
+    baseURL: 'http://0d43ca75.ngrok.io/api',
     headers: {
       authorization: token
     }
