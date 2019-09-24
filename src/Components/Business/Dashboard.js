10970a54c460dd640afe87f0e9b34679dba3c18e
Gurpreet Dhaliwal <gdhaliwal22@gmail.com>
2019-09-24 14:19:10 -0700

    added schedule div to dashboard

--- src/Components/Business/Dashboard.js
+++ src/Components/Business/Dashboard.js
@@ -15,18 +15,40 @@ const BusinessDashboard = () => {
                     <p className='location-address'>123 Street<br />San Francisco, CA 94103</p>
                     </div>
                 </div>
-                <button className='dashboard-button add-location__button'>
-                
-                New<br />Location
-                </button>
+                <button className='dashboard-button add-location__button'>New Location</button>
             </div>
             </div>
             <div className='dashboard-section'>
             <h3 className='dashboard-subheader'>Scheduled Pickups</h3>
-            <button className='dashboard-button'>
-                + Create a Pickup
-                </button>
+            <button className='dashboard-button'>+ Create a Pickup</button>
+            </div>
+            <div className='dashboard-section'>
+            <h3 className='dashboard-subheader'>Next Week's Schedule</h3>
 
+            <div className="week-schedule">
+                <div className="day-schedule">
+                    <div className='day-schedule__date'>September 23, 2019</div>
+                {/* map over appointments here */}
+                <div className='day-schedule__pickup'>
+                        <div className="pickup-info">
+                            <div className="pickup-time-amount">4:00 PM - 10 lbs</div>
+                            <div className="pickup-type">Chicken and steak</div>
+                        </div>
+                    </div>
+                    </div>
+                <div className="day-schedule">
+                    <div className='day-schedule__date'>September 24, 2019</div>
+                </div>
+                <div className="day-schedule">
+                    <div className='day-schedule__date'>September 25, 2019</div>
+                </div>
+                <div className="day-schedule">
+                    <div className='day-schedule__date'>September 26, 2019</div>
+                </div>
+                <div className="day-schedule">
+                    <div className='day-schedule__date'>September 27, 2019</div>
+                </div>
+                </div>
             </div>
         </div>
     )
