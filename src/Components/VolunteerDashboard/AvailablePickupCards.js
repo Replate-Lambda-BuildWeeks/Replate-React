a24b53a880e67ea6e8aad937a15ea4f1c6b17f11
Arielle Hollingsworth <arizephyr123@gmail.com>
2019-09-25 13:42:20 -0700

    VolunteerDashboard progress and components displaying pickups available or already claimed by volunteer

---
+++ src/Components/VolunteerDashboard/AvailablePickupCards.js
@@ -0,0 +1,20 @@
+import React from "react";
+import { Card, CardTitle, CardSubtitle, Button } from "reactstrap";
+
+export default function AvailablePickupCards({ quantity, food, date, time }) {
+    // function formatDate(){
+    //     const formated = "";
+
+    //     return()
+    // }
+  return (
+    <div className="pickupCard">
+      <Card>
+        <CardTitle>{`${date} at ${time}`}</CardTitle>
+        {/* <CardSubtitle>{`Business Name here ->` + name}</CardSubtitle> */}
+        <CardSubtitle>{`${quantity} of ${food}`}</CardSubtitle>
+        <Button type="submit">Claim</Button>
+      </Card>
+    </div>
+  );
+}
--- src/Components/VolunteerDashboard/VolunteerDashboard.js
+++ src/Components/VolunteerDashboard/VolunteerDashboard.js
@@ -1,78 +1,94 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
-import PickupCards from "./PickupCards";
+import ScheduledPickupCards from "./ScheduledPickupCards";
+import AvailablePickupCards from "./AvailablePickupCards";
 import logo from "../../images/logo.png";
-
+import userImage from "../../images/userImage.jpg";
 
 export default function VolunteerDashboard() {
   const [pickup, setPickup] = useState([]); //holding pickup data
   const [volunteer, setVolunteer] = useState(["Volunteer"]); // state holding volunteer name
-  const [business, setBusiness] = useState([]); // state holding business data: address, etc
+  // const [business, setBusiness] = useState([]); // state holding business data: address, etc
 
-//axios call for volunteer name
-  useEffect(() =>{
+  //axios call for volunteer name
+  useEffect(() => {
     axios
-    .get(`http://0bbfee1e.ngrok.io/volunteers/1`)
-    .then(res => {
-      console.log(res.data.volunteer_name);
-      setVolunteer(res.data);
-      // const VolName = res.data.name;
-    })
-    .catch(err => {
-      console.log("Volunteer Dashboard:", err);
-    });
+      .get(`http://0bbfee1e.ngrok.io/volunteers/2`)
+      .then(res => {
+        console.log(res.data.volunteer_name);
+        setVolunteer(res.data);
+        // const VolName = res.data.name;
+      })
+      .catch(err => {
+        console.log("Volunteer Dashboard:", err);
+      });
   }, [setVolunteer]);
   //axios call for business pickups
-  useEffect(() =>{
+  useEffect(() => {
     axios
-    .get("http://0bbfee1e.ngrok.io/pickups")
-    .then(res => {
-      //console.log(res.data);
-      setPickup(res.data);
-    })
-    .catch(err => {
-      console.log(err);
-    });
+      .get("http://0bbfee1e.ngrok.io/pickups")
+      .then(res => {
+        console.log(res.data);
+        setPickup(res.data);
+      })
+      .catch(err => {
+        console.log(err);
+      });
   }, [volunteer]);
-  
-  return (
-    <div className="dashboard">
-      <h1 className="dashboard-header">REPLATE</h1>
-      <img src={logo} alt="logo"/>
-      <div className="dashboard-section">
-        <h3 className="dashboard-subheader">Welcome {volunteer.volunteer_name}!</h3>
-        <div className="dashboard-locations">
-          <div className="dashboard-location">
-            <div className="location-image"></div>
-          </div>
-        </div>
-      </div>
-      <div className="dashboard-section">
-        <h3 className="dashboard-subheader">Scheduled Pickups</h3>
-        <button className="dashboard-button"> Claim a Pickup</button>
-        <div className="day-schedule__pickup">
-              {/* {pickup.map(p => {
-                // spread each of the pickup values as props into a card component:
-                return <PickupCards key = {p.id} {...p}/>
-              })} */}
-            </div>
 
+  //axios call for business address
+  useEffect(() => {
+    axios
+      .get("http://0bbfee1e.ngrok.io/pickups")
+      .then(res => {
+        console.log(res.data);
+        setPickup(res.data);
+      })
+      .catch(err => {
+        console.log(err);
+      });
+  }, [volunteer]);
+
+  // this function renders list of pickups available to claim. 
+  function Available() {
+    pickup.map(p => {
+      if (p.volunteer_id === volunteer.volunteer_id) {
+        // spread each of the pickup values as props into a card component:
+        console.log(p);
+        console.log("IN Available()");
+        return( <AvailablePickupCards key={p.id} {...p} />)
+      } else if (p.volunteer_id != null) {
+        return <h2>None</h2>;
+      }
+    });
+  }
+
+  return (
+    <div className="volDash">
+      <h1 className="volDash-header">REPLATE</h1>
+      <img className="volDash-logo" src={logo} alt="logo" />
+      <div className="volDash-UserSection">
+        <h3 className="volDash-welcome">Welcome {volunteer.volunteer_name}!</h3>
+        <img className="volDash-userImage" src={userImage} alt="user" />
+        <button onClick={Available} className="volDash-button">
+          Claim a Pickup
+        </button>
       </div>
-      <div className="dashboard-section">
-        <h3 className="dashboard-subheader">Available Pickups</h3>
-        <div className="week-schedule">
-          <div className="day-schedule">
-            <div className="day-schedule__date">Today</div>
-            {/* map over appointments here */}
-            <div className="day-schedule__pickup">
-              {pickup.map(p => {
-                if (p.volunteer_id === null){
-                // spread each of the pickup values as props into a card component:
-                console.log(p);
-                return <PickupCards key = {p.id} {...p}/>}
-              })}
-            </div>
-          </div>
+      <div className="volDash-section">
+        <h3>Your Scheduled Pickups</h3>
+        <div className="volDash-day-schedule__pickup">
+          {pickup.map(p => {
+            if (p.volunteer_id === volunteer.id) {
+              // spread each of the pickup values as props into a card component:
+              // console.log(volunteer.volunteer_id);
+              // console.log(p);
+              return <ScheduledPickupCards key={p.id} {...p} />;
+            } else if (p.volunteer_id != null) {
+              // console.log(volunteer.id);
+              // console.log(p.volunteer_id);
+              return null;
+            } 
+          })}
         </div>
       </div>
     </div>
